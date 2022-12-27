2dc02b38d463b80dcb5b6f054f8ae1a0e0f025ec
Jagdish Pote SL <jagdishpote02@gmail.com>
2022-12-27 23:50:40 +0530

    First commit

---
+++ Project_Git_Intro/src/com/udemy/gitIntro/ApplicationOne.java
@@ -0,0 +1,13 @@
+package com.udemy.gitIntro;
+
+public class ApplicationOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Application, first built");
+		System.out.println("no changes here");
+	
+
+	}
+
+}
---
+++ Project_Git_Intro/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author potej
+ *
+ */
+module Project_Git_Intro {
+}
