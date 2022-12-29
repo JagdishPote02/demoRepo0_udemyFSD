6213e54d1c20d00ffc8344048389e2f521860e5b
Jagdish Pote SL <jagdishpote02@gmail.com>
2022-12-29 20:20:43 +0530

    2nd commit with changes in file v1.2

--- Project_Git_Intro/src/com/udemy/gitIntro/ApplicationOne.java
+++ Project_Git_Intro/src/com/udemy/gitIntro/ApplicationOne.java
@@ -6,6 +6,12 @@ public class ApplicationOne {
 		// TODO Auto-generated method stub
 		System.out.println("Application, first built");
 		System.out.println("no changes here");
+		
+		//chenges here***********
+		System.out.println("successfully uploaded project with above code, first commit with access token"
+				+ "here is the change will need to commit again as a second commit"
+				+ "...we can download project with download option and import zip file to eclipse"
+				+ "");
 	
 
 	}
